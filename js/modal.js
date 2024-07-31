147df8f331455d32bf074568d136436147119ba8
YunnDian <71434288+yunndian27@users.noreply.github.com>
2024-07-31 13:53:01 +0800

    Update modal.js
    多字刪除

--- js/modal.js
+++ js/modal.js
@@ -255,7 +255,7 @@ modal ='<!-- Modal -->\
     <div class="modal-dialog modal-dialog-centered modal-lg">\
       <div class="modal-content">\
         <div class="modal-header">\
-          <h5 class="modal-title" id="choke02ModalLabel">傳口字型扁銅線 CM choke</h5>\
+          <h5 class="modal-title" id="choke02ModalLabel">口字型扁銅線 CM choke</h5>\
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>\
         </div>\
         <div class="modal-body">\
